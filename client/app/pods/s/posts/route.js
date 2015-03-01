d5b278145880c4a62823c32d5ea08401d59fbea9
e-karma <djbradyla@gmail.com>
2015-03-01 03:00:23 -0800

    nested posts for create and read

--- client/app/pods/s/posts/route.js
+++ client/app/pods/s/posts/route.js
@@ -1,4 +1,9 @@
-import Ember from 'ember';
 
-export default Ember.Route.extend({
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model: function () {
+		return this.store.find('post');
+	}
 });
